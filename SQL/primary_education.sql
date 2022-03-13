8c40aa220115e26e5abb4e2719a26d5e052efec7
Ildem <99615234+ildemsanli@users.noreply.github.com>
2022-03-13 12:38:25 +0100

    Add files via upload

--- SQL/primary_education.sql
+++ SQL/primary_education.sql
@@ -14,19 +14,22 @@ TimePeriod integer);
 select * from education; 
 
 #1
-select Country, Region, IncomeGroup, rank() over(order by Total desc) as Ranking from education;
+select Country, Region, IncomeGroup, rank() over(order by Total desc) as Ranking from education limit 10;
 
 
 #2
 SELECT Region, round(AVG(Total), 2), round(avg(Poorest), 2), round(avg(Richest),2) 
 FROM education GROUP BY Region; 
 #or 
-SELECT Region, round(AVG(Total), 2), round(avg(Richest)/avg(Poorest),2) 
+SELECT Region, round(AVG(Total), 2) as Total, round(avg(Richest)/avg(Poorest),2) as 'Richest/Poorest'
 FROM education GROUP BY Region; 
 
 #3
-SELECT IncomeGroup, round(avg(Total),2), round(avg(ResRural), 2), round(avg(ResUrban), 2)
+SELECT IncomeGroup, round(avg(Total),2) as Total, round(avg(ResRural), 2) as Rural, round(avg(ResUrban), 2) as Urban
 FROM education GROUP BY IncomeGroup order by avg(Total) desc ; 
+#or
+SELECT IncomeGroup, round(avg(Total),2) as Total, round(avg(ResUrban)/avg(ResRural), 2) as 'Urban/Rural'
+FROM education GROUP BY IncomeGroup order by avg(Total) desc ;
 
 
 
